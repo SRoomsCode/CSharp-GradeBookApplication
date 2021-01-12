2eac01b85961aa7b028d94709f8ebcdfecfb5e6b
Steve <srooms@outlook.com>
2021-01-12 20:33:17 +0100

    Add method GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,15 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade) 
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("There should be at least 5 students");
+            }
+
+            return 'F';
+        }
     }
 }
